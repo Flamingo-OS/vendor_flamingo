AOSPA-legacy/android_vendor_pa

vendor: copy latinime lib to device system
- also updated latinime lib

Change-Id: If3e4342414faa5307ebb8000ae17a7fa7bfb8b6f

configs/aosp_fixes.mk
@@ -30,3 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so

Flamingo-OS/vendor_flamingo

configs/aosp_fixes.mk
@@ -31,3 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so