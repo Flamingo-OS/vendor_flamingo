namdeptrai2003/android_vendor_dnd

sepolicy: allow vold to create files on external sdcard
This is required for ASEC support. Vold can already create and
access directories, but do not yet have the permission for files.

Change-Id: I5082bbff692e5dc53c7000e4b3a293e42d33f901

sepolicy/sepolicy.mk
@@ -13,4 +13,5 @@ BOARD_SEPOLICY_UNION += \
     genfs_contexts \
     installd.te \
     seapp_contexts \
+    vold.te \
     mac_permissions.xml

Flamingo-OS/vendor_flamingo

sepolicy/sepolicy.mk
@@ -18,4 +18,5 @@ BOARD_SEPOLICY_UNION += \
     seapp_contexts \
     service_contexts \
     system.te \
-    zygote.te
+    zygote.te \
+    vold.te