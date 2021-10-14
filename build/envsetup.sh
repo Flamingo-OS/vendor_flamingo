AOSPA/android_vendor_aospa

aospa: build: Add a script to merge CAF.
Change-Id: I1d36d27be1bf476c951e04de75c9ea85b80af9ab

build/envsetup.sh
@@ -1,0 +1,14 @@
+function __print_aospa_functions_help() {
+cat <<EOF
+Additional Paranoid Android functions:
+- cafmerge:        Utility to merge CAF tags.
+EOF
+}
+
+function cafmerge()
+{
+    target_branch=$1
+    set_stuff_for_environment
+    T=$(gettop)
+    python3 $T/vendor/pa/scripts/merge-caf.py $target_branch
+}

Flamingo-OS/vendor_flamingo

build/envsetup.sh
@@ -1,0 +1,14 @@
+function __print_aospa_functions_help() {
+cat <<EOF
+Additional Paranoid Android functions:
+- cafmerge:        Utility to merge CAF tags.
+EOF
+}
+
+function cafmerge()
+{
+    target_branch=$1
+    set_stuff_for_environment
+    T=$(gettop)
+    python3 $T/vendor/aospa/scripts/merge-caf.py $target_branch
+}