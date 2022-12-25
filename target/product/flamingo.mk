67049cea63bd7d01bec6ee45ee71668bad7eb28c
jhonboy121 <alfredmathew05@gmail.com>
2022-12-25 06:23:44 +0000

    configs: allow overriding signing keys
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- target/product/flamingo.mk
+++ target/product/flamingo.mk
@@ -13,8 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(strip $(SIGNING_KEYS)),)
 ifneq ($(wildcard certs/releasekey.*),)
 SIGNING_KEYS := certs/releasekey
+endif
 endif
 
 ifeq ($(strip $(OFFICIAL_BUILD)),true)
